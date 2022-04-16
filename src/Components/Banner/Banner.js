98bdd7857c0239c16e9e9faf91459eae1be2117d
RKZeebon <rkzeebon@gmail.com>
2022-04-16 13:09:03 -0700

    banner area done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -3,9 +3,12 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className='banner-bg'>
-            <div className='px-10 py-10'>
-                <h1 className='text-4xl'>Keep your memory safe!</h1>
+        <div className='banner-bg relative'>
+            <div className='px-10 py-10 absolute right-0 left-0 bottom-20 text-white'>
+                <div className='text-center leading-8'>
+                    <h1 className='text-4xl uppercase font-Anek'>Keep your <span className='text-orange-400'>memory</span> safe!</h1>
+                    <p className='text-xl'>I am here to capture your spacial moment to give you a nostalogic feel in your future.</p>
+                </div>
             </div>
         </div >
     );
