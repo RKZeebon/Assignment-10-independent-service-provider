6ee41d6a3a3a0579507405a0942c9c3176f2d62c
RKZeebon <rkzeebon@gmail.com>
2022-04-17 13:53:45 -0700

    env local done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBMeltBnFsSXe4-KTy-EKq2bAJT2XDVbA4",
-  authDomain: "rkz-photography18.firebaseapp.com",
-  projectId: "rkz-photography18",
-  storageBucket: "rkz-photography18.appspot.com",
-  messagingSenderId: "615690105953",
-  appId: "1:615690105953:web:f345560cc1cac9c0a5e620"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
