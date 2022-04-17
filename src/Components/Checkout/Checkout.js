2ddb0b2defd0a86e31521ba9e88bbb409114f718
RKZeebon <rkzeebon@gmail.com>
2022-04-17 14:13:45 -0700

    check out page done

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useServices from '../Hooks/UseServices';
 
 const Checkout = () => {
     const [submit, setSubmit] = useState(false)
+    const { serviceId } = useParams()
+    const [services] = useServices()
+    const selectedService = services.find(service => service.id == serviceId)
 
     const handleCheckOut = event => {
         event.preventDefault()
         setSubmit(true)
-
     }
 
     return (
@@ -16,11 +20,7 @@ const Checkout = () => {
                 <div className='flex justify-center'>
 
                     <form onSubmit={handleCheckOut} action="" className='w-full lg:w-1/4 block'>
-                        <div>
-                            {
-                                submit && <h1 className='text-2xl font-semibold'>Thanks For the booking.</h1>
-                            }
-                        </div>
+
                         <input className='border-black my-4 border-2 block p-3 rounded-lg text-2xl w-full' type="text" name="name" placeholder='Your Name' required />
 
                         <input className='border-black my-4 border-2 block p-3 rounded-lg text-2xl w-full' type="email" name="email" placeholder="Your Email" required />
@@ -29,6 +29,11 @@ const Checkout = () => {
 
                         <input className='border-black my-4 border-2 block p-3 rounded-lg text-2xl w-full' type="text" name="phone" placeholder="Your Phone Number" required />
 
+                        <div>
+                            {
+                                submit && <h1 className='text-2xl font-semibold text-center'>Thanks For the booking <br /><span className='font-bold text-orange-600'>{selectedService.title}</span>.</h1>
+                            }
+                        </div>
 
                         <input className='my-4 block p-3 rounded-lg w-full bg-orange-400 text-2xl font-Anek font-semibold cursor-pointer' type="submit" value="Submit" />
                     </form>
--- src/Components/Hooks/UseServices.js
+++ src/Components/Hooks/UseServices.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useServices = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('Services.json')
+        fetch('https://gist.githubusercontent.com/RKZeebon/0653d6c165df22cfb7ff6703f97921e0/raw/3dad3a43bba7691110554e0fd1231b18ed577b28/Services.json')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
