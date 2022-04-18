d4b2033c3d59e3eb8be985c3ca001078e6e8cb6b
RKZeebon <rkzeebon@gmail.com>
2022-04-18 02:49:35 -0700

    everything ok

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -7,20 +7,30 @@ const Checkout = () => {
     const { serviceId } = useParams()
     const [services] = useServices()
     const selectedService = services.find(service => service.id == serviceId)
-
+    console.log(selectedService);
     const handleCheckOut = event => {
         event.preventDefault()
         setSubmit(true)
     }
 
     return (
-        <div className='min-h-[850px]'>
-            <div className='w-3/4 mx-auto'>
-
-                <div className='flex justify-center'>
-
-                    <form onSubmit={handleCheckOut} action="" className='w-full lg:w-1/4 block'>
-
+        <div className='min-h-[850px] mt-12'>
+            <div className='w-3/4 mx-auto lg:grid grid-cols-2'>
+                <div className='shadow-2xl shadow-orange-200 p-3 rounded-lg lg:w-3/4 mx-auto'>
+                    <div className='rounded-lg mx-auto min-h-[650px]'>
+                        <img className='mb-3 rounded-t-lg max-h-[450px] mx-auto' src={selectedService?.img} alt="" />
+                        <div className='lg:flex lg:justify-center'>
+                            <div>
+                                <h1 className='text-4xl'>{selectedService?.title}</h1>
+                                <p>{selectedService?.text}</p>
+                                <h3 className='text-2xl'>${selectedService?.price}</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex justify-center mt-16'>
+                    <form onSubmit={handleCheckOut} action="" className='w-full lg:w-3/4'>
+                        <h1 className='text-3xl font-Anek'>Please fill out the form to check out.</h1>
                         <input className='border-black my-4 border-2 block p-3 rounded-lg text-2xl w-full' type="text" name="name" placeholder='Your Name' required />
 
                         <input className='border-black my-4 border-2 block p-3 rounded-lg text-2xl w-full' type="email" name="email" placeholder="Your Email" required />
